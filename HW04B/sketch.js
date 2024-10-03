b39d1e08aabc47de3097c904eae870b89e74396f
Lauren Tsao <et2676@nyu.edu>
2024-10-03 17:25:20 -0400

    moved SL sketch from 04A to 04B

--- HW04B/sketch.js
+++ HW04B/sketch.js
@@ -1,11 +1,117 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function emitter(x, y, rx, ry) {
+  push();
+  translate(x, y);
+
+  line(0, 0, rx, ry);
+
+  pop();
 }
 
-function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(250);
+  strokeWeight(1);
+  angleMode(DEGREES);
+
+  print(width, height);
+  let blue = color(38, 91, 170);
+  let red = color(205, 40, 40);
+  let yellow = color(244, 231, 41);
+
+  //top left emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(blue);
+    let rx = random(500, 1000);
+    let ry = random(0, 1000);
+    emitter(0, 0, rx, ry);
+  }
+
+  //top middle emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(red);
+    let rx = random(-500, 1000);
+    let ry = random(-500, 500);
+    emitter(width / 2, 0, rx, ry);
+  }
+
+  //top right emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(blue);
+    let rx = random(-500, -1000);
+    let ry = random(0, 500);
+    emitter(width, 0, rx, ry);
+  }
+
+  //middle left emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(red);
+    let rx = random(0, 1000);
+    let ry = random(-1000, 1000);
+    emitter(0, height / 2, rx, ry);
+  }
+
+  //true middle emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    stroke(yellow);
+    let rx = random(-1000, 1000);
+    let ry = random(-1000, 1000);
+    emitter(width / 2, height / 2, rx, ry);
+  }
+
+  //middle right emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(red);
+    let rx = random(-500, -1000);
+    let ry = random(-500, 1000);
+    emitter(width, height / 2, rx, ry);
+  }
+
+  //bottom left emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(blue);
+    let rx = random(500, 1000);
+    let ry = random(0, -1000);
+    emitter(0, height, rx, ry);
+  }
+
+  //bottom middle emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(red);
+    let rx = random(-1000, 500);
+    let ry = random(0, -1000);
+    emitter(width / 2, height, rx, ry);
+  }
+
+  //bottom right emitter
+  for (let cnt = 0; cnt < 10; cnt++) {
+    stroke(blue);
+    let rx = random(-500, -1000);
+    let ry = random(-500, -1000);
+    emitter(width, height, rx, ry);
+  }
+
+  // // draw a column of rows
+  // for (let y = 0; y < height; y += 1) {
+  //   // draw a row of circle
+  //   for (let x = 0; x < width; x += 1) {
+  //     let dd = random(1, 3); //drawing dimension, randomize size between 16 to sqDim
+  //     let vx = random(-5, 5); //vx = variation in x btw the numbers -30 to 30
+  //     let vy = random(-5, 5); //vy = variation in y btw the numbers -30 to 30
+  //     // let rf = random(90, 80);
+  //     push();
+  //     noStroke(200);
+  //     fill(50);
+  //     translate(x, y);
+  //     blendMode(SCREEN);
+  //     ellipse(vx, vy, dd);
+  //     pop();
+  //   }
+  // }
 }
+
+function draw() {}
+
+function mouseClicked() {
+  setup();
+}
+
