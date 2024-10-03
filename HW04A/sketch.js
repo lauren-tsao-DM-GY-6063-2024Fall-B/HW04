5586a6113db8d1ad4bc5f323b266bacc165bd866
Lauren Tsao <et2676@nyu.edu>
2024-10-03 00:20:20 -0400

    Completed design

--- HW04A/sketch.js
+++ HW04A/sketch.js
@@ -1,12 +1,99 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function emitter(x, y) {
+  push();
+  translate(x, y);
+
+  let rx = random(-1000, 1000);
+  let ry = random(-1000, 1000);
+
+  line(0, 0, rx, ry);
+
+  pop();
 }
 
-function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(235);
+  strokeWeight(2);
+  angleMode(DEGREES);
+
+  print(width, height);
+  let blue = color(38, 91, 170)
+  let red = color(205, 40, 40)
+  let yellow = color(244, 231, 41)
+
+  //top left emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(0, 0);
+    
+  }
+
+  //top middle emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(width / 2, 0);
+    stroke(205, 40, 40)
+  }
+
+  //top right emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    push()
+    print(cnt);
+    emitter(width, height);
+    stroke(38, 91, 170)
+    pop()
+  }
+
+  //middle left emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    push()
+    print(cnt);
+    emitter(0, height / 2);
+    stroke(205, 40, 40)
+    pop()
+  }
+
+  //true middle emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(width / 2, height / 2);
+    stroke(244, 231, 41)
+  }
+
+  //middle right emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(0, height / 2);
+    stroke(205, 40, 40)
+  }
+
+  //bottom left emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(0, height);
+    stroke(38, 91, 170)
+  }
+
+  //bottom middle emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(width / 2, height);
+    stroke(205, 40, 40)
+  }
+
+  //bottom right emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(width, 0);
+    stroke(38, 91, 170)
+  }
+
+  //middle right emitter
+  for (let cnt = 0; cnt < 19; cnt++) {
+    print(cnt);
+    emitter(width, height / 2);
+    stroke(205, 40, 40)
+  }
 }
+
+function draw() {}
